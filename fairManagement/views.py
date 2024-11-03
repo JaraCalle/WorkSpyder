8445f55b993e7e28d7b85dd9a11f1395728acf4b
masalgaro <124186064+masalgaro@users.noreply.github.com>
2024-11-02 19:19:34 -0500

    Properly adds and deletes favorites in db

--- fairManagement/views.py
+++ fairManagement/views.py
@@ -24,8 +24,23 @@ def register_fair(request):
     registration = FairRegistration(aspirant=aspirant, fair=fair, registrationDate=timezone.now().date())
     registration.save()
     return redirect('successful_inscription', registration_id=registration.id, fair_title=fair.title)
-    
-    
+
+    def favorite_fair(request):
+    if request.method == "POST":
+        aspirant_id = request.POST.get('aspirant_id')
+        fair_id = request.POST.get('fair_id')
+        aspirant = Aspirant.objects.get(id=aspirant_id)
+        fair = JobFair.objects.get(id=fair_id)
+
+        if FairFavorite.objects.filter(aspirant=aspirant, fair=fair).exists():
+            FairFavorite.objects.filter(aspirant=aspirant, fair=fair).delete()
+        else:
+            favorite = FairFavorite(aspirant=aspirant, fair=fair, favoriteDate=timezone.now().date())
+            favorite.save()
+
+        return redirect("view_fairs")
+
+
 
 def error_inscripcion(request, message):
     return render(request, 'error-inscripcion.html', {'message': message})
