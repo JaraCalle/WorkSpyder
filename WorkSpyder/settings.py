b67322170b4f8d5be28e25ac395a9d072cb66d3f
Juan Jara <jaracalle2041@gmail.com>
2024-11-05 12:58:21 -0500

    [HOT FIX] Solves the media and static problem with the release

--- WorkSpyder/settings.py
+++ WorkSpyder/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static",  # Asegúrate de que sea el directorio de archivos estáticos de desarrollo
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # La carpeta en la que se recopilarán los archivos estáticos para producción
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
