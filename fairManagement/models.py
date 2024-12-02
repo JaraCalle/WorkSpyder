3d47aef5a1692ba25e2d61add66b8f8676bfeacf
Juan Jara <jaracalle2041@gmail.com>
2024-12-02 11:04:28 -0500

    Implements the regex for the phone aspirant model

--- fairManagement/models.py
+++ fairManagement/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.conf import settings
 from exploreFairs.models import JobFair
+from django.core.validators import RegexValidator
 
 class Aspirant(models.Model):
     id = models.AutoField(primary_key = True)
     firstName = models.CharField(max_length=50)
     secondName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=100)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{10}$',  # Expresión regular para exactamente 10 dígitos
+                message="El número de teléfono debe tener exactamente 10 dígitos.",
+                code='invalid_phone'
+            )
+        ]
+    )
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
     def is_aspirant_data_empty(self) -> bool:
